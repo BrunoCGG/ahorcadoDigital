c2d15449bc6611e40e6eb827bd1a845e4e7b5929
Bruno <bcgarciag@gmail.com>
2017-02-17 15:47:46 -0500

    se agrega la funcionalidad de validaPalabra y numFallas

--- lib/ahorcadoDigital.rb
+++ lib/ahorcadoDigital.rb
@@ -4,7 +4,8 @@ class AhorcadoDigital
 		@palabras = ["sombrilla","bola","automovil","jarra","elefante","perro","mujer","scrum","libreta","agua"]
 		@palabra = palabra
 		@palabraOculta = palabra
-		@palabraDescubierta = palabra
+		@fallas = 0
+		@LIMITE_FALLAS = 5 
     end
 
     def obtenerPalabra
@@ -18,12 +19,34 @@ class AhorcadoDigital
         return @palabraOculta
     end
 
+    def numFallas
+    	return @fallas
+    end
+
+    def validaPalabra
+    	if (expect(@palabra).to eq @palabraOculta)
+    	   return "GANASTE"
+    	else
+    	   if @numFallas < @LIMITE_FALLAS
+     	   	 return "SIGUE INTENTANDO"
+     	   else
+     	   	 return "HAS SIDO AHORCADO"
+     	   end
+    	end
+    end
+
     def validaLetra letra
+    	localizado = 0
 		for i in 0..@palabra.length-1
 			if(@palabra[i] == letra)
 				@palabraOculta[i] = letra
+				localizado = localizado + 1
 			end
 		end
+		if localizado = 0
+	    	@fallas = @fallas + 1
+	    end
+	    return localizado
 	end
 
 	private
